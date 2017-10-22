816ebe42a3688197c7127361b9911f90ba5155e4
implose <void@implo.se>
2017-10-22 16:12:58 +0200

    add xhr promise function

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,1 +1,17 @@
 console.warn('I have become self aware, human.')
+
+/**
+ * Make a XHR call
+ * @param {string} url 
+ * @param {boolean} post 
+ */
+function XHR(url, post=false){
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    if(post){xhr.open('POST', url);}
+    else{xhr.open('GET', url);}
+    xhr.onload = () => resolve(xhr.responseText);
+    xhr.onerror = () => reject(xhr.statusText);
+    xhr.send();
+  });
+}
