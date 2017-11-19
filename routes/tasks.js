731df0791d095904ae280d18b456a74e5dc0ac52
Yorick <yorick@ctrlatldev.xyz>
2017-11-19 23:30:44 +0100

    update

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,26 +2,10 @@
 var conf = require('../conf');
 var express = require('express');
 var router = express.Router();
-var r = require('rethinkdbdash')({
-  servers:[
-    {
-      host: conf.database.host,
-      port: conf.database.port,
-      user: conf.database.user,
-      password: conf.database.password,
-      db: conf.database.database
-    }
-  ],
-  silent: true
-});
 
 // GET: /
 router.get('/', function(req, res) {
-  r.table('tasks').run().then((result) => {
-    res.json(result);
-  }).catch((error) => {
-    console.log(error);
-  });
+  
 });
 
 module.exports = router;
