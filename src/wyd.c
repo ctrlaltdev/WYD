06024c3b2c742517a4be36ba2fb8b42f86d8f758
Yorick <yorick@ctrlaltdev.xyz>
2018-03-06 14:28:01 +0100

    fully functionnal

--- src/wyd.c
+++ src/wyd.c
@@ -7,11 +7,17 @@ void help() {
 	fprintf(stdout, "usage: wyd command\n" \
 	"	commands:\n" \
 	"		add \"Task\"		alias of create\n" \
+	"		close ID		mark a task as done\n" \
 	"		create \"Task\"		create a task\n" \
+	"		delete ID		delete a task\n" \
+	"		done ID			alias of close\n" \
 	"		init			create the database\n" \
 	"		list [options]		list the open tasks\n" \
 	"			-a		list all tasks\n" \
 	"		ls			alias of list\n" \
+	"		rm ID			alias of delete\n" \
+	"		ud ID \"Task\"		alias of update\n" \
+	"		update ID \"Task\"	update task content\n" \
 	);
 }
 
@@ -24,7 +30,7 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
 	 return 0;
 }
 
-int createDB(int argc, char* argv[]) {
+int createDB() {
 	 sqlite3 *db;
 	 char *zErrMsg = 0;
 	 int rc;
@@ -48,80 +54,28 @@ int createDB(int argc, char* argv[]) {
 	 fprintf(stderr, "SQL error: %s\n", zErrMsg);
 			sqlite3_free(zErrMsg);
 	 } else {
-			fprintf(stdout, "Database created\n");
+			fprintf(stdout, "What You Doin\' initialized\n");
 	 }
 	 sqlite3_close(db);
 	 return 0;
 }
 
-int create(char* task) {
-	sqlite3 *db;
-	int rc;
-	sqlite3_stmt *stmt;
-
-	rc = sqlite3_open_v2("./.WYD.db", &db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL);
-	
-	if ( rc ) {
-		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-		return(1);
-	}
-
-	sqlite3_prepare_v2(db, "INSERT INTO TASKS (TASK) VALUES(?1);", -1, &stmt, NULL);
-	sqlite3_bind_text(stmt, 1, task, -1, SQLITE_STATIC);
-	rc = sqlite3_step(stmt); 
-	if (rc != SQLITE_DONE) {
-		printf("ERROR inserting data: %s\n", sqlite3_errmsg(db));
-		return 1;
-	}
-
-	sqlite3_finalize(stmt);
-	printf("Task created.\n");
-	sqlite3_close(db);
-	return 0;
-}
-
-int delete(int id) {
-	sqlite3 *db;
-	int rc;
-	sqlite3_stmt *stmt;
-
-	rc = sqlite3_open_v2("./.WYD.db", &db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL);
-	
-	if ( rc ) {
-		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-		return(1);
-	}
-
-	sqlite3_prepare_v2(db, "DELETE FROM TASKS WHERE ID = ?1;", -1, &stmt, NULL);
-	sqlite3_bind_int(stmt, 1, id);
-	rc = sqlite3_step(stmt); 
-	if (rc != SQLITE_DONE) {
-		printf("ERROR deleting data: %s\n", sqlite3_errmsg(db));
-		return 1;
-	}
-
-	sqlite3_finalize(stmt);
-	printf("Task deleted.\n");
-	sqlite3_close(db);
-	return 0;
-}
-
-int list(int showall) {
+void list(int showall) {
 	sqlite3 *db;
 	int rc;
 	int id;
 	const unsigned char* task;
+	int status;
 	sqlite3_stmt *stmt;
 
-	rc = sqlite3_open_v2("./.WYD.db", &db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL);
+	rc = sqlite3_open_v2("./.WYD.db", &db, SQLITE_OPEN_READONLY, NULL);
 	
 	if ( rc ) {
 		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-		return(1);
 	}
 
 	if ( showall == 1 ) {
-		sqlite3_prepare_v2(db, "SELECT ID, TASK FROM TASKS;", -1, &stmt, NULL);
+		sqlite3_prepare_v2(db, "SELECT ID, TASK, STATUS FROM TASKS;", -1, &stmt, NULL);
 	}
 	else {
 		sqlite3_prepare_v2(db, "SELECT ID, TASK FROM TASKS WHERE STATUS = 0;", -1, &stmt, NULL);
@@ -131,9 +85,22 @@ int list(int showall) {
 	while ( done == 0 ) {
 		switch (sqlite3_step (stmt)) {
 			case SQLITE_ROW:
-				id = sqlite3_column_int(stmt, 0);
-				task  = sqlite3_column_text(stmt, 1);
-				printf ("#%d - %s\n", id, task);
+				if ( showall == 1 ) {
+					id = sqlite3_column_int(stmt, 0);
+					task  = sqlite3_column_text(stmt, 1);
+					status = sqlite3_column_int(stmt, 2);
+					if (status == 1) {
+						printf ("#%d - [X] %s\n", id, task);
+					}
+					else {
+						printf ("#%d - [ ] %s\n", id, task);
+					}
+				}
+				else {
+					id = sqlite3_column_int(stmt, 0);
+					task  = sqlite3_column_text(stmt, 1);
+					printf ("#%d - [ ] %s\n", id, task);
+				}
 				break;
 
 			case SQLITE_DONE:
@@ -142,43 +109,151 @@ int list(int showall) {
 
 			default:
 				fprintf(stderr, "Failed.\n");
-				return 1;
 			}
 	}
 
 	sqlite3_finalize(stmt);
 	sqlite3_close(db);
-	return 0;
+}
+
+void create(char* task) {
+	sqlite3 *db;
+	int rc;
+	sqlite3_stmt *stmt;
+
+	rc = sqlite3_open_v2("./.WYD.db", &db, SQLITE_OPEN_READWRITE, NULL);
+	
+	if ( rc ) {
+		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+	}
+
+	sqlite3_prepare_v2(db, "INSERT INTO TASKS (TASK) VALUES(?1);", -1, &stmt, NULL);
+	sqlite3_bind_text(stmt, 1, task, -1, SQLITE_STATIC);
+	rc = sqlite3_step(stmt); 
+	if (rc != SQLITE_DONE) {
+		printf("ERROR creating task: %s\n", sqlite3_errmsg(db));
+	}
+
+	sqlite3_finalize(stmt);
+	sqlite3_close(db);
+	list(0);
+}
+
+void delete(int id) {
+	sqlite3 *db;
+	int rc;
+	sqlite3_stmt *stmt;
+
+	rc = sqlite3_open_v2("./.WYD.db", &db, SQLITE_OPEN_READWRITE, NULL);
+	
+	if ( rc ) {
+		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+	}
+
+	sqlite3_prepare_v2(db, "DELETE FROM TASKS WHERE ID = ?1;", -1, &stmt, NULL);
+	sqlite3_bind_int(stmt, 1, id);
+	rc = sqlite3_step(stmt); 
+	if (rc != SQLITE_DONE) {
+		fprintf(stderr, "ERROR deleting task: %s\n", sqlite3_errmsg(db));
+	}
+
+	sqlite3_finalize(stmt);
+	sqlite3_close(db);
+	list(0);
+}
+
+void done(int id) {
+	sqlite3 *db;
+	int rc;
+	sqlite3_stmt *stmt;
+
+	rc = sqlite3_open_v2("./.WYD.db", &db, SQLITE_OPEN_READWRITE, NULL);
+	
+	if ( rc ) {
+		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+	}
+
+	sqlite3_prepare_v2(db, "UPDATE TASKS SET STATUS = 1 WHERE ID = ?1;", -1, &stmt, NULL);
+	sqlite3_bind_int(stmt, 1, id);
+	rc = sqlite3_step(stmt); 
+	if (rc != SQLITE_DONE) {
+		fprintf(stderr, "ERROR closing task: %s\n", sqlite3_errmsg(db));
+	}
+
+	sqlite3_finalize(stmt);
+	sqlite3_close(db);
+	list(0);
+}
+
+void update(int id, char* task) {
+	sqlite3 *db;
+	int rc;
+	sqlite3_stmt *stmt;
+
+	rc = sqlite3_open_v2("./.WYD.db", &db, SQLITE_OPEN_READWRITE, NULL);
+	
+	if ( rc ) {
+		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+	}
+
+	sqlite3_prepare_v2(db, "UPDATE TASKS SET TASK = ?1 WHERE ID = ?2;", -1, &stmt, NULL);
+	sqlite3_bind_text(stmt, 1, task, -1, SQLITE_STATIC);
+	sqlite3_bind_int(stmt, 2, id);
+	rc = sqlite3_step(stmt);
+	if (rc != SQLITE_DONE) {
+		fprintf(stderr, "ERROR updating task: %s\n", sqlite3_errmsg(db));
+	}
+
+	sqlite3_finalize(stmt);
+	sqlite3_close(db);
+	list(0);
 }
 
 int main(int argc, char* argv[]) {
 
 	if( argc == 2 ) {
 		if ( strcmp(argv[1], "init") == 0 ) {
-			createDB(argc, argv);
-		}
-		else if ( strcmp(argv[1], "create") == 0  || strcmp(argv[1], "add") == 0 ) {
-			create(argv[2]);
+			createDB();
 		}
 		else if ( strcmp(argv[1], "list") == 0  || strcmp(argv[1], "ls") == 0 ) {
 			list(0);
+			return 0;
 		}
 		else {
 			help();
+			return 0;
 		}
 	}
 	else if ( argc == 3 ) {
 		if ( strcmp(argv[1], "create") == 0  || strcmp(argv[1], "add") == 0 ) {
 			create(argv[2]);
+			return 0;
 		}
 		else if ( ( strcmp(argv[1], "list") == 0  || strcmp(argv[1], "ls") == 0 ) && strcmp(argv[2], "-a") == 0 ) {
 			list(1);
+			return 0;
 		}
 		else if ( strcmp(argv[1], "delete") == 0  || strcmp(argv[1], "rm") == 0 ) {
 			delete(atoi(argv[2]));
+			return 0;
+		}
+		else if ( strcmp(argv[1], "close") == 0  || strcmp(argv[1], "done") == 0 ) {
+			done(atoi(argv[2]));
+			return 0;
 		}
 		else {
 			help();
+			return 0;
+		}
+	}
+	else if ( argc == 4 ) {
+		if ( strcmp(argv[1], "update") == 0  || strcmp(argv[1], "ud") == 0 ) {
+			update(atoi(argv[2]),argv[3]);
+			return 0;
+		}
+		else {
+			help();
+			return 0;
 		}
 	}
 	else {
